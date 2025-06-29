77de64993e7c7cdae781e8ce6ddd5a675e2005e8
gourav <kartikpal1402@gmail.com>
2025-06-29 18:43:29 +0530

    DB-mongoose setup

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const connectDB = () =>{
+    mongoose.connect(process.env.MONGODB_URI)
+      .then(() => console.log("MongoDB connected"))
+      .catch((err) => console.error("MongoDB connection error:", err));
+}
+
+module.exports = connectDB;
+
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./db");
 const authRoutes = require('./routes/auth');
+
+dotenv.config();
 
 const corsOptions = {
     origin: ["http://localhost:5173"],
 }
 app.use(cors(corsOptions));
+app.use(express.json());
+
+connectDB();
 
 app.use('/auth', authRoutes);
 
-
-app.listen(8080, () => {
-    console.log("Server is listening to port 8080");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log("Server is listening to port ", PORT);
 })
