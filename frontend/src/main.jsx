8a57d84e09721f8987a7826ef92a537497f68853
gourav <kartikpal1402@gmail.com>
2026-01-30 19:41:50 +0530

    add: context access

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,18 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { MockProvider } from "./context/Mock.jsx"
+import { RoomProvider } from "./context/Room.jsx";
+import { MessageProvider } from "./context/Message.jsx";
 
 createRoot(document.getElementById("root")).render(
     <BrowserRouter>
-        <App />
+        <MockProvider>
+            <RoomProvider>
+                <MessageProvider>
+                    <App />
+                </MessageProvider>
+            </RoomProvider>
+        </MockProvider>
     </BrowserRouter>
 );
