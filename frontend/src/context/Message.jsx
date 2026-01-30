46aa71c2baabeb80d7c07c8c69ee94a754617d92
gourav <kartikpal1402@gmail.com>
2026-01-30 19:40:51 +0530

    add: message context for messages related operations

---
+++ frontend/src/context/Message.jsx
@@ -0,0 +1,49 @@
+import { createContext, useContext, useState } from "react";
+import { RoomContext } from "./Room";
+import { getRoomMessages } from "../api/room";
+import { emitChatMessage } from "../sockets/room.emits";
+import { MockContext } from "./Mock";
+
+export const MessageContext = createContext(null);
+
+export const MessageProvider = ({ children }) => {
+    const MockContextValue = useContext(MockContext);
+    const { currentUser } = MockContextValue;
+    const RoomContextValue = useContext(RoomContext);
+    const { activeRoom } = RoomContextValue;
+    const [messages, setMessages] = useState([]);
+
+    const senderName = currentUser?.riotId?.gameName || "Unknown";
+    const isOwner = currentUser?.puuid === activeRoom.createdBy?.puuid;
+
+    console.log("this is messages", messages);
+    const fetchMessages = async () => {
+        try {
+            const res = await getRoomMessages(activeRoom._id);
+            const data = await res.json();
+            if (data.success && Array.isArray(data.messages)) {
+                setMessages(data.messages);
+            }
+        } catch (err) {
+            console.error("Failed to load messages:", err);
+        }
+    };
+    const handleChatMessage = (msg) => {
+        setMessages((prev) => [...prev, msg]);
+    };
+    const handleSendMessage = (msg) => {
+        if (!msg.trim()) return;
+
+        emitChatMessage(activeRoom._id, senderName, msg);
+        // setInput("");
+    };
+    const value = {
+        messages,
+        senderName,
+        isOwner,
+        fetchMessages,
+        handleChatMessage,
+        handleSendMessage
+    };
+    return <MessageContext.Provider value={value}> {children} </MessageContext.Provider>;
+};
