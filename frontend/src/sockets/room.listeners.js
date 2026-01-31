ce85741e6d1961872ea45d0c7dbd9bcf5a69787d
gourav <kartikpal1402@gmail.com>
2026-01-31 14:33:37 +0530

    add: new listeners for room related io emits

--- frontend/src/sockets/room.listeners.js
+++ frontend/src/sockets/room.listeners.js
@@ -1,9 +1,29 @@
 import socket from "./socket";
 
-export const listenRoomUpdates = (callback) => {
-    socket.on("roomUpdated", callback);
-    return () => socket.off("roomUpdated", callback);
-};
+// export const listenRoomUpdates = (callback) => {
+//     socket.on("roomUpdated", callback);
+//     return () => socket.off("roomUpdated", callback);
+// };
+
+export const listenRoomCreated = (callback) => {
+    socket.on("roomCreated", callback);
+    return () => socket.off("roomCreated", callback);
+}
+
+export const listenRoomDeleted = (callback) => {
+    socket.on("roomDeleted", callback);
+    return () => socket.off("roomDeleted", callback);
+}
+
+export const listenRoomJoined = (callback) => {
+    socket.on("roomJoined", callback);
+    return () => socket.off("roomJoined", callback);
+}
+
+export const listenRoomLeft = (callback) => {
+    socket.on("roomLeft", callback);
+    return () => socket.off("roomLeft", callback);
+}
 
 export const listenLeaveRoomAll = (callback) => {
     socket.on("leaveRoomAll", callback);
